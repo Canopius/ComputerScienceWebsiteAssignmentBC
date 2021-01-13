fa8e920ec48913482ac2777080fc15653c8bb13c
Josh <joshuagrey981@gmail.com>
2021-01-13 10:44:42 +0000

    Audio work

--- javascript/main.js
+++ javascript/main.js
@@ -1,7 +1,15 @@
-var audio = new Audio('..//Assets/FastTalking.mp3');
-audio.play();
+var audio1 = new Audio('..//Assets/FastTalking1.mp3');
+var audio2 = new Audio('..//Assets/FastTalking2.mp3');
+var audio3 = new Audio('..//Assets/FastTalking3.mp3');
 
-audio.oncanplaythrough = function(){
-    audio.play();
+function toggleAudio(img, audio){
+    if(img.src.match(/Play/)){
+        audio.play()
+        img.src = "../Assets/Pause.svg";
+    }else{
+        audio.pause()
+        img.src = "../Assets/Play.svg";
+    }
 }
+
 
