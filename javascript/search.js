078bd21991b812f9c63867fa4ecfbf21478b6a1b
Josh <joshuagrey981@gmail.com>
2021-02-01 11:23:32 +0000

    Career search work

--- javascript/search.js
+++ javascript/search.js
@@ -3,21 +3,60 @@ var allCareers = [  [["Entry level web developer", "PHP", "HTML", "CSS", "Databa
                     [["Frontend Programmer", "HTML", "CSS"], ["£40,000"]]
 ];
 
-var a = 5
-
 function searchCareers(){
     var ToFind  = document.getElementById("SearchBox").value
     var ToShow = []
-    console.log(ToFind);
     if (ToFind.length > 0){
         for( var i = 0, len = allCareers.length; i < len; i++ ) {
             for(var index = 0, length = allCareers[i][0].length; index < length; index++ ){
                 if(allCareers[i][0][index].toLowerCase().includes(ToFind.toLowerCase()) || ToFind.toLowerCase() == allCareers[i][0][index].toLowerCase() ){         
-                    ToShow.splice(0, 0, allCareers[i][0][0]);
+                    ToShow.splice(0, 0, allCareers[i]);
                     break;
                 }
             }
         }
     }
-    document.getElementById("SearchForm").submit();
+    
+    if (ToShow.length > 0){
+        document.getElementById("mainHome").remove();
+        document.getElementById("mainImg").remove();
+        document.getElementById("content").style.gridTemplateColumns = "0.25fr 0.25fr 0.25fr 0.25fr";
+        document.getElementById("content").style.gridTemplateRows= "repeat(auto, 1fr)";
+        document.getElementById("content").style.gridGap = "20px";
+
+        for (var i = 0, len = ToShow.length; i < len; i ++){
+            console.log(ToShow[i]);
+            
+            const a = document.createElement('a');
+            const img = document.createElement('img');
+            const title = document.createElement('h4');
+            const pay = document.createElement('h4');
+            
+            const titleText = document.createTextNode(ToShow[i][0][0]);
+            title.appendChild(titleText);
+
+            const payText = document.createTextNode(ToShow[i][1][0]);
+            pay.appendChild(payText);
+            
+            img.src = "../Assets/Logo.svg"
+
+            document.querySelector('.content').append(a);
+            a.append(img);
+            a.append(title);
+            a.append(pay);
+
+            if (i%2 == 0){ // Every other one
+                a.className = "careerCardL"
+            }else{
+                a.className = "careerCardR"
+            }
+
+            console.log("DONE")
+        }
+        return true;
+    }else{
+        // Show something along the lines of no items mached your search
+        return false;
+    }
+    
 };
